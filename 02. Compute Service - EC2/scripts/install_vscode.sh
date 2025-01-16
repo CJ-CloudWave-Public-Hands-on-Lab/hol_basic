1667b498859e4f3b876a2a27e94a446487d5730a
seunghyun.you <sh1517.you@gmail.com>
2025-01-16 11:29:59 +0900

    Update install_vscode.sh

--- 02. Compute Service - EC2/scripts/install_vscode.sh
+++ 02. Compute Service - EC2/scripts/install_vscode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set environment variables
 AWS_REGION="ap-northeast-2"
-AWS_ACCOUNT_ID="602229900482"
+AWS_ACCOUNT_ID="*************"
 USER_EMAIL="cj-cloud-wave@workshops.aws"
 USER_NAME="Workshop Public Cloud"
 PASSWORD="CJ_CloudWave2025!!"
